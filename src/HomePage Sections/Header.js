dad1c4adb1b74cbf8e04fcde2eebe7a31f121a5c
Tomás Fernandes <fernandesstomas@gmail.com>
2024-01-17 17:21:51 +0000

    responsive nav done

--- src/HomePage Sections/Header.js
+++ src/HomePage Sections/Header.js
@@ -1,6 +1,47 @@
 import { Link } from "react-scroll";
+import { useRef } from "react";
 
 export default function Header() {
+  const menuIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="ionicon"
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-miterlimit="10"
+        stroke-width="32"
+        d="M80 160h352M80 256h352M80 352h352"
+      />
+    </svg>
+  );
+
+  const closeIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="ionicon"
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="32"
+        d="M368 368L144 144M368 144L144 368"
+      />
+    </svg>
+  );
+
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive-nav");
+  };
+
   return (
     <header className="header">
       <div className="header-main-div">
@@ -13,7 +54,7 @@ export default function Header() {
           <p className="logo-text">Tomás Fernandes</p>
         </div>
 
-        <nav>
+        <nav ref={navRef}>
           <ul className="nav-ul">
             <li className="nav-li">
               <Link to="intro-section" smooth={true}>
@@ -34,7 +75,13 @@ export default function Header() {
               <div>My CV</div>
             </li>
           </ul>
+          <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+            {closeIcon}
+          </button>
         </nav>
+        <button className="nav-btn nav-menu-btn" onClick={showNavBar}>
+          {menuIcon}
+        </button>
       </div>
     </header>
   );
